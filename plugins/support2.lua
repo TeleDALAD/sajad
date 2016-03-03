3d17fc39fc9d23a4eab40ac49eaf4930050c56a4
TeleDALAD <ashkankorouei@gmail.com>
2016-03-03 15:00:59 +0330

    Create support2.lua

---
+++ plugins/support2.lua
@@ -0,0 +1,38 @@
+do
+
+local function callback(extra, success, result)
+  vardump(success)
+  vardump(result)
+end
+
+local function run(msg, matches)
+
+  local user = 175623013
+
+  if matches[1] == "add abol" then
+    user = 'user#id'..user
+  end
+
+  -- The message must come from a chat group
+  if msg.to.type == 'chat' then
+    local chat = 'chat#id'..msg.to.id
+    chat_add_user(chat, user, callback, false)
+    return "سودوی ربات وارد گروه شد"
+    return 'This isnt a chat group!'
+  end
+
+end
+
+return {
+  description = "support", 
+  patterns = {
+    "^[!/](add sajad)$"
+  }, 
+  run = run 
+}
+
+end
+--Copyright; @behroozyaghi
+--Persian Translate; @behroozyaghi
+--ch : @nod32team
+--کپی بدون ذکر منبع حرام است
